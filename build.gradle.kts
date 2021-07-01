f19f6be9d8700b59586238d1e0d7c61ba366fecd
ssrsph@naver.com <ssrsph@naver.com>
2021-07-01 16:07:11 +0900

    chore: apply kotlin DSL to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:${BuildDependencyVersions.GRADLE}"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildDependencyVersions.KOTLIN}"
-        classpath "com.google.dagger:hilt-android-gradle-plugin:${BuildDependencyVersions.HILT}"
-        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:${BuildDependencyVersions.NAVIGATION}"
+        classpath ("com.android.tools.build:gradle:${BuildDependencyVersions.GRADLE}")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:${BuildDependencyVersions.KOTLIN}")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:${BuildDependencyVersions.HILT}")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:${BuildDependencyVersions.NAVIGATION}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -23,6 +23,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
