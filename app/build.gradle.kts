90b98cd9dead4c7ffe2dafa67c4c0694b4ec3338
ssrsph@naver.com <ssrsph@naver.com>
2021-07-05 03:04:33 +0900

    feat: create marker, mapview toggle button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
     id(BuildPlugins.ANDROID_APPLICATION)
     id(BuildPlugins.KOTLIN_ANDROID)
     id(BuildPlugins.KOTLIN_KAPT)
+    id(BuildPlugins.HILT_PLUGIN)
     id(BuildPlugins.NAVIGATION_SAFE_ARGS)
     id(BuildPlugins.PARCELIZE)
 }
-//
+
 val properties = Properties()
 properties.load(FileInputStream(rootProject.file("./local.properties")))
 
@@ -92,6 +93,9 @@ android {
 
         implementation(Dependencies.ROOM)
         kapt(Dependencies.ROOM_COMPILER)
+
+        implementation(Dependencies.HILT)
+        kapt(Dependencies.HILT_COMPILER)
 
         implementation(Dependencies.NAVER_MAP)
 
--- app/src/main/java/com/caffeine/capin/MapFragment.kt
+++ app/src/main/java/com/caffeine/capin/MapFragment.kt
@@ -2,27 +2,61 @@ package com.caffeine.capin
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.caffeine.capin.databinding.FragmentMapBinding
-import com.caffeine.capin.preference.UserPreferenceManager
 import com.caffeine.capin.util.AutoClearedValue
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import dagger.hilt.android.AndroidEntryPoint
 
-
-class MapFragment: Fragment() {
+@AndroidEntryPoint
+class MapFragment: Fragment(), OnMapReadyCallback {
     private var binding by AutoClearedValue<FragmentMapBinding>()
+    private lateinit var naverMap: NaverMap
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMapBinding.inflate(layoutInflater, container, false)
+        mapView = binding.mapview
+        mapView.getMapAsync(this)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        setMarker(37.580221,126.923442)
+        setMarker(37.579174,126.923809)
+    }
+
+    private fun setMarker(lat: Double, long : Double) {
+        val marker = Marker()
+        Log.e("marker", "marker")
+        marker.icon = OverlayImage.fromResource(R.drawable.marker_capinmap_unselected)
+        marker.position = LatLng(lat, long)
+        val cameraUpdate = CameraUpdate.scrollTo(marker.position).animate(CameraAnimation.Easing)
+        marker.map = naverMap
+        naverMap.moveCamera(cameraUpdate)
+        naverMap.contentBounds
+    }
+
+    private fun getCurrentZoomBound() {
+        naverMap.addOnCameraChangeListener(object: NaverMap.OnCameraChangeListener{
+            override fun onCameraChange(reason: Int, animated: Boolean) {
+                
+            }
+        })
     }
 }
