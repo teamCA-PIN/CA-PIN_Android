1812aab34c563b29a491bc4b6f9f2bcda5762e96
ssrsph@naver.com <ssrsph@naver.com>
2021-07-07 02:16:13 +0900

    fix: delete dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,10 +107,4 @@ android {
         androidTestImplementation(TestDependencies.ESPRESSO)
     }
 }
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra["kotlin_version"]}")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-}
 
