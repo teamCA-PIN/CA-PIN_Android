d1ee8bcdd5a83cae26dbfbd953be84770a49009b
ssrsph@naver.com <ssrsph@naver.com>
2021-07-06 14:38:48 +0900

    feat: switch capin map, my map

--- app/src/main/java/com/caffeine/capin/MapFragment.kt
+++ app/src/main/java/com/caffeine/capin/MapFragment.kt
@@ -1,11 +1,13 @@
 package com.caffeine.capin
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.caffeine.capin.databinding.FragmentMapBinding
 import com.caffeine.capin.util.AutoClearedValue
 import com.naver.maps.geometry.LatLng
@@ -18,6 +20,7 @@ import kotlin.collections.ArrayList
 @AndroidEntryPoint
 class MapFragment: Fragment(), OnMapReadyCallback {
     private var binding by AutoClearedValue<FragmentMapBinding>()
+    private val viewModel by viewModels<MapViewModel>()
     private lateinit var naverMap: NaverMap
     private lateinit var mapView: MapView
     private var activeMarkers = ArrayList<Marker>()
@@ -35,55 +38,65 @@ class MapFragment: Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+
     }
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+        setCafeInformation()
         changeMap()
     }
 
-    private fun changeMap() {
-        binding.radiogroupMap.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener{
-            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-                when(checkedId) {
-                    binding.radiobuttonMyMap.id -> {
-                        getCurrentZoomBound(myMapPositions)
-                    }
-                    binding.radiobuttonCapinMap.id -> {
-                        getCurrentZoomBound(capinMapPositions)
-                    }
-                }
-            }
-        })
-    }
-
-    private fun getCurrentZoomBound(markersPosition: ArrayList<LatLng>) {
-        naverMap.addOnCameraChangeListener { reason, animated ->
-            freeActiveMarkers()
-            getCameraBounds(markersPosition)
+    private fun setCafeInformation() {
+        viewModel.cafeInformation.observe(viewLifecycleOwner) {
+            setMarkersInsideCamera()
+            setCameraChangeListener()
         }
     }
 
-    private fun getCameraBounds(markersPosition: ArrayList<LatLng>) {
+    private fun changeMap() {
+        binding.radiogroupMap.setOnCheckedChangeListener { _, checkedId ->
+            freeActiveMarkers()
+            when (checkedId) {
+                binding.radiobuttonMyMap.id -> {
+                    viewModel.switchToMyMap()
+                }
+                binding.radiobuttonCapinMap.id -> {
+                    viewModel.switchToCapinMap()
+                }
+            }
+        }
+    }
+
+    private fun setCameraChangeListener() {
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            freeActiveMarkers()
+            setMarkersInsideCamera()
+        }
+    }
+
+    private fun setMarkersInsideCamera() {
         val eastBound = naverMap.coveringBounds.eastLongitude
         val westBound = naverMap.coveringBounds.westLongitude
         val northBound = naverMap.coveringBounds.northLatitude
         val southBound = naverMap.coveringBounds.southLatitude
 
-        markersPosition.forEach { markerPosition ->
-            if (markerPosition.longitude in westBound..eastBound) {
-                if(markerPosition.latitude in southBound..northBound) {
-                    setMarker(markerPosition)
+        viewModel.cafeInformation.value?.forEach { location ->
+            if (location.longtitude in westBound..eastBound) {
+                if(location.latitude in southBound..northBound) {
+                    setMarker(LatLng(location.latitude, location.longtitude))
                 }
             }
         }
     }
 
-    private fun setMarker(position: LatLng) {
+    private fun setMarker(location: LatLng) {
         val marker = Marker()
-        marker.position = position
+        marker.position = location
         marker.icon = OverlayImage.fromResource(R.drawable.marker_capinmap_unselected)
         marker.map = naverMap
+        Log.e("marker", "${marker.position}")
         activeMarkers.add(marker)
     }
 
@@ -91,27 +104,5 @@ class MapFragment: Fragment(), OnMapReadyCallback {
         activeMarkers.forEach{ marker ->
             marker.map = null
         }
-    }
-
-    companion object {
-        private val capinMapPositions: ArrayList<LatLng> = arrayListOf<LatLng>(
-            LatLng(37.580221,
-                126.923442),
-            LatLng(37.582996109622876,
-                126.91380431146156),
-            LatLng(37.579256039764246,
-                126.92016389612074),
-            LatLng(
-                37.556561236976535,
-                126.92786145335195
-            )
-        )
-
-        private val myMapPositions: ArrayList<LatLng> = arrayListOf(
-            LatLng(37.5598780330203, 126.92397328447223),
-            LatLng(37.5628956716561, 126.92529371146098),
-            LatLng(37.56111453790112, 126.92340389420406),
-            LatLng(37.56216407332653, 126.92306471146108)
-        )
     }
 }
