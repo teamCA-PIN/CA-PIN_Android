70fce43424a42141693cc31a859f5e6e696f9c08
ssrsph@naver.com <ssrsph@naver.com>
2021-07-08 22:27:14 +0900

    feat: create map cafe cardview layout

--- app/src/main/java/com/caffeine/capin/util/BindingAdapter.kt
+++ app/src/main/java/com/caffeine/capin/util/BindingAdapter.kt
@@ -3,11 +3,19 @@ package com.caffeine.capin.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
 
 object BindingAdapter {
     @BindingAdapter("load_url")
     @JvmStatic
     fun ImageView.loadImageView(url: String?) {
         Glide.with(context).load(url).into(this)
+    }
+
+    @BindingAdapter("app:useCircleOutlineRadius")
+    @JvmStatic
+    fun ShapeableImageView.useCircleOutline(radius: Float) {
+        shapeAppearanceModel = ShapeAppearanceModel().withCornerSize(radius)
     }
 }
