a3b6757aa7c3a0225344e494752f312c17dc6ef3
ssrsph@naver.com <ssrsph@naver.com>
2021-07-09 23:21:21 +0900

    feat: create card view

--- app/src/main/java/com/caffeine/capin/map/CafeInformationEntity.kt
+++ app/src/main/java/com/caffeine/capin/map/CafeInformationEntity.kt
@@ -1,6 +1,11 @@
 package com.caffeine.capin.map
 
 data class CafeInformationEntity(
+    val name: String,
     val latitude: Double,
-    val longtitude: Double
+    val longtitude: Double,
+    val tag: String,
+    val address: String,
+    val cafeImage: String,
+    val rating: Double
 )
