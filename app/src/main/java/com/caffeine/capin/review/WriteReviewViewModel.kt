d80ea8a39d30d9d3689a78fe5e9992a4b2ecb5df
ssrsph@naver.com <ssrsph@naver.com>
2021-07-12 06:06:37 +0900

    feat: 리뷰작성 뷰작업 완료

--- app/src/main/java/com/caffeine/capin/review/WriteReviewViewModel.kt
+++ app/src/main/java/com/caffeine/capin/review/WriteReviewViewModel.kt
@@ -1,5 +1,6 @@
 package com.caffeine.capin.review
 
+import android.graphics.Picture
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,4 +8,32 @@ import androidx.lifecycle.ViewModel
 class WriteReviewViewModel: ViewModel() {
     val rateOfReview = MutableLiveData<Float>(0.0f)
     val contentsOfReview = MutableLiveData<String>()
+
+    private val cafePhotos = mutableListOf<PictureUriEntity>()
+
+    private val _imagesOfCafe = MutableLiveData<List<PictureUriEntity>>()
+    val imagesOfCafe: LiveData<List<PictureUriEntity>>
+        get() = _imagesOfCafe
+
+    fun addImagesOfCafe(pictureUriEntity: PictureUriEntity) {
+        cafePhotos.add(pictureUriEntity)
+        _imagesOfCafe.value = cafePhotos
+    }
+
+    fun deleteImageOfCafe(pictureUriEntity: PictureUriEntity) {
+        cafePhotos.remove(pictureUriEntity)
+        _imagesOfCafe.value = cafePhotos
+    }
+
+    fun checkImagesCount(): Boolean {
+        return if (imagesOfCafe.value.isNullOrEmpty()) {
+            true
+        } else {
+            imagesOfCafe.value!!.size < 5
+        }
+    }
+
+    fun switchPostButtonActivation(): Boolean {
+        return rateOfReview.value != 0.0f && !(contentsOfReview.value.isNullOrBlank())
+    }
 }
