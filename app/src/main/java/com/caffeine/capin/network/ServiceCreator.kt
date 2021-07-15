a8d9347743b0444fbb4b4dacce2d91209997bfef
Cho Sungrim <srcho730@gmail.com>
2021-07-15 19:22:59 +0900

    feat: mypage profile api

--- app/src/main/java/com/caffeine/capin/network/ServiceCreator.kt
+++ app/src/main/java/com/caffeine/capin/network/ServiceCreator.kt
@@ -6,6 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceCreator {
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
