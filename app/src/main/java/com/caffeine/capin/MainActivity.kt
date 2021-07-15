f8fee4c0f19dec57caf50e2fe209a0cf38c0bd7a
ssrsph@naver.com <ssrsph@naver.com>
2021-07-15 21:05:28 +0900

    fix: 토큰 제거

--- app/src/main/java/com/caffeine/capin/MainActivity.kt
+++ app/src/main/java/com/caffeine/capin/MainActivity.kt
@@ -24,9 +24,7 @@ class MainActivity: AppCompatActivity() {
         setContentView(binding.root)
         setNavController()
         transparentStatusAndNavigation()
-
-        userPreferenceManager.setUserToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MGVkNDM0YmUxMGI5ZDA3ZWQ1MmRiNzkiLCJpYXQiOjE2MjYzNDU5ODUsImV4cCI6MTYyNjQzMjM4NX0.-t0fysp9BJG1fibSiHHWQpvfKkCJ7iYtEw0Ai9S0olY")
-
+        
     }
 
     private fun setNavController() {
