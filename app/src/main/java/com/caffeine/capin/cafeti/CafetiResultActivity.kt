be888b428dd8eef864490b4683fab2c2539df19b
ssrsph@naver.com <ssrsph@naver.com>
2021-07-16 11:44:30 +0900

    feat: 카테고리에 카페 추가 수정

--- app/src/main/java/com/caffeine/capin/cafeti/CafetiResultActivity.kt
+++ app/src/main/java/com/caffeine/capin/cafeti/CafetiResultActivity.kt
@@ -2,18 +2,18 @@ package com.caffeine.capin.cafeti
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.caffeine.capin.MainActivity
 import com.caffeine.capin.databinding.ActivityCafetiResultBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.caffeine.capin.preference.UserPreferenceManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class CafetiResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCafetiResultBinding
+    @Inject lateinit var userPreferenceManager: UserPreferenceManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +27,7 @@ class CafetiResultActivity : AppCompatActivity() {
 
     private fun updateCafetiResult() {
         if (intent.hasExtra("cafeti_result")) {
+            userPreferenceManager.setNeedCafetiCheck(false)
             val cafetiResultList = intent.getSerializableExtra("cafeti_result") as ResponseCafetiData.Result
             Glide.with(binding.imageViewCafetiResult.context).load(cafetiResultList.img).into(binding.imageViewCafetiResult)
             binding.textViewCafetiType.text = cafetiResultList.type
