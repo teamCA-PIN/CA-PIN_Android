d69a0f93037dbb797f64252b39d571dab3763c43
CHOSUNGRIM <srcho730@gmail.com>
2021-12-22 21:03:55 +0900

    chore: 중간 커밋

--- app/src/main/java/com/caffeine/capin/mypage/ui/MyPageProfileEditActivity.kt
+++ app/src/main/java/com/caffeine/capin/mypage/ui/MyPageProfileEditActivity.kt
@@ -1,8 +1,11 @@
 package com.caffeine.capin.mypage.ui
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -15,24 +18,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.caffeine.capin.R
 import com.caffeine.capin.customview.CapinDialog
 import com.caffeine.capin.customview.CapinDialogBuilder
 import com.caffeine.capin.customview.CapinDialogButton
 import com.caffeine.capin.customview.CapinToastMessage
 import com.caffeine.capin.databinding.ActivityMyPageProfileEditBinding
+import com.caffeine.capin.databinding.ActivityMyPageProfileEditBindingImpl
 import com.caffeine.capin.mypage.api.response.ResponseMyData
 import com.caffeine.capin.network.BaseResponse
 import com.caffeine.capin.network.ServiceCreator
 import com.caffeine.capin.preference.UserPreferenceManager
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okio.BufferedSink
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.io.File
+import java.io.FileOutputStream
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -42,12 +53,15 @@ class MyPageProfileEditActivity : AppCompatActivity() {
     lateinit var userPreferenceManager: UserPreferenceManager
 
     private lateinit var binding: ActivityMyPageProfileEditBinding
-    private lateinit var path: String
+    private var path: String = ""
+    private lateinit var tempBitmap: Bitmap
+    private lateinit var nicknameBody: MultipartBody.Part
+    private lateinit var profileImageBody: MultipartBody.Part
 
     private val getImageLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             binding.profileEditProfileIv.setImageURI(result.data?.data)
-            if(result.data != null) {
+            if (result.data != null) {
                 path = absolutelyPath(result.data?.data!!)
             }
         }
@@ -65,7 +79,8 @@ class MyPageProfileEditActivity : AppCompatActivity() {
             showEditProfileDialog()
         }
         binding.profileEditDoneBtn.setOnClickListener {
-            putMyProfileEditToServer() }
+            putMyProfileEditToServer()
+        }
     }
 
     private fun setEditTextWatcher() {
@@ -149,6 +164,22 @@ class MyPageProfileEditActivity : AppCompatActivity() {
                         .with(binding.profileEditProfileIv.context)
                         .load(response.body()?.myInfo?.profileImg)
                         .into(binding.profileEditProfileIv)
+
+                    Glide.with(this@MyPageProfileEditActivity)
+                        .asBitmap()
+                        .load(response.body()?.myInfo?.profileImg)
+                        .into(object : CustomTarget<Bitmap>() {
+                            override fun onResourceReady(
+                                resource: Bitmap,
+                                transition: Transition<in Bitmap>?
+                            ) {
+                                binding.profileEditProfileIv.setImageBitmap(resource)
+                                tempBitmap = resource
+                            }
+
+                            override fun onLoadCleared(placeholder: Drawable?) {
+                            }
+                        })
                 }
             }
         })
@@ -157,7 +188,7 @@ class MyPageProfileEditActivity : AppCompatActivity() {
     private fun chekGalleryPermission() {
         if (ContextCompat.checkSelfPermission(
                 this,
-                Companion.PERMISSION_READ_EXTERNAL_STORAGE
+                PERMISSION_READ_EXTERNAL_STORAGE
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             requestGalleryPermission.launch(PERMISSION_READ_EXTERNAL_STORAGE)
@@ -180,50 +211,97 @@ class MyPageProfileEditActivity : AppCompatActivity() {
         }
 
     private fun putMyProfileEditToServer() {
-        val file = File(path)
+        nicknameBody = if (binding.profileEditNameEdt.text.isNullOrBlank()) {
+            MultipartBody.Part.createFormData(
+                "nickname",
+                binding.profileEditNameEdt.hint.toString()
+            )
+        } else {
+            MultipartBody.Part.createFormData(
+                "nickname",
+                binding.profileEditNameEdt.text.toString()
+            )
+        }
 
-        var requestBody : RequestBody = RequestBody.create("image/jpeg".toMediaTypeOrNull(),file)
-        var body : MultipartBody.Part = MultipartBody.Part.createFormData("profileImg",file.name,requestBody)
-        var nicknameBody : MultipartBody.Part = MultipartBody.Part.createFormData("nickname", binding.profileEditNameEdt.text.toString())
+        if (path == "") {
+            CapinToastMessage.createCapinRejectToast(
+                this@MyPageProfileEditActivity,
+                "프로필 이미지는 필수 입력 항목입니다.",
+                100
+            )?.show()
+            return
+            //fixme
+//            Log.d("리미 bitmap", "init if")
+//            val existingRequestBody : RequestBody = BitmapRequestBody(tempBitmap)
+//            Log.d("리미 나한테 왜 그래 existing", existingRequestBody.toString())
+//            profileImageBody = MultipartBody.Part.createFormData("profileImg", "", existingRequestBody)
+//            Log.d("리미 나한테 왜 그래 profile", profileImageBody.toString())
+        } else {
+            val file = File(path)
+            val requestBody : RequestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            profileImageBody = MultipartBody.Part.createFormData("profileImg", file.name, requestBody)
+        }
 
         val capinApiService = ServiceCreator.capinApiService.putMyProfileEdit(
             userPreferenceManager.getUserAccessToken(),
             nickname = nicknameBody,
-            profileImg = body
+            profileImg = profileImageBody
         )
 
         capinApiService.enqueue(object : Callback<BaseResponse> {
             override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
                 Log.d("fail", "error:$t")
-                CapinToastMessage.createCapinRejectToast(
-                    this@MyPageProfileEditActivity,
-                    "사용할 수 없는 이름입니다.",
-                    100
-                )?.show()
             }
 
             override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
                 if (response.isSuccessful) {
                     finish()
+                } else {
+                    CapinToastMessage.createCapinRejectToast(
+                        this@MyPageProfileEditActivity,
+                        "사용할 수 없는 이름입니다.",
+                        100
+                    )?.show()
                 }
             }
         })
     }
 
-    fun absolutelyPath(path: Uri): String {
-
-        var proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
-        var c: Cursor = contentResolver.query(path, proj, null, null, null)!!
-        var index = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+    private fun absolutelyPath(path: Uri): String {
+        val proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
+        val c: Cursor = contentResolver.query(path, proj, null, null, null)!!
+        val index = c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
         c.moveToFirst()
 
-        var result = c.getString(index)
-
-        return result
+        return c.getString(index)
     }
+
+    fun getFileFromBitmap(bitmap: Bitmap, context: Context): File? {
+        val cache = context.externalCacheDir
+        val shareFile = File(cache, "myFile.jpg")
+        try {
+            val out = FileOutputStream(shareFile)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.flush()
+            out.close()
+            return shareFile
+        } catch (e: Exception) {
+
+        }
+        return null
+    }
+
 
     companion object {
         private const val PERMISSION_READ_EXTERNAL_STORAGE =
             android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    class BitmapRequestBody(private val bitmap: Bitmap) : RequestBody() {
+        override fun contentType(): MediaType? = "image/jpeg".toMediaTypeOrNull()
+
+        override fun writeTo(sink: BufferedSink) {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 99, sink.outputStream())
+        }
     }
 }
