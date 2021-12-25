44b46ef3de4ce9d7f2f9ed1f98a1e2e47eca25f6
CHOSUNGRIM <srcho730@gmail.com>
2021-12-26 04:38:49 +0900

    refactor: 프로필 편집 수정 중

--- app/src/main/java/com/caffeine/capin/mypage/ui/MyPageProfileEditActivity.kt
+++ app/src/main/java/com/caffeine/capin/mypage/ui/MyPageProfileEditActivity.kt
@@ -3,6 +3,7 @@ package com.caffeine.capin.mypage.ui
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -11,10 +12,12 @@ import android.text.TextWatcher
 import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.caffeine.capin.R
 import com.caffeine.capin.customview.CapinDialog
 import com.caffeine.capin.customview.CapinDialogBuilder
@@ -29,11 +32,26 @@ import dagger.hilt.android.AndroidEntryPoint
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.File
 import javax.inject.Inject
+import com.bumptech.glide.load.engine.GlideException
+
+import com.bumptech.glide.request.RequestListener
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import java.lang.Exception
+import android.graphics.Bitmap.CompressFormat
+import android.graphics.drawable.Drawable
+import android.os.Environment
+import android.os.Handler
+import android.os.Looper
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import java.io.*
+
 
 @AndroidEntryPoint
 class MyPageProfileEditActivity : AppCompatActivity() {
@@ -42,12 +60,18 @@ class MyPageProfileEditActivity : AppCompatActivity() {
     lateinit var userPreferenceManager: UserPreferenceManager
 
     private lateinit var binding: ActivityMyPageProfileEditBinding
-    private lateinit var path: String
+    private var path: String? = ""
+    private lateinit var profileImageBody: MultipartBody.Part
+    private lateinit var nicknameBody: MultipartBody.Part
+    private var tempNickname: String? = null
+    private var tempCafeti: String? = null
+    private var tempExistingImage: String? = null
+    private var tempFile: File? = null
 
     private val getImageLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             binding.profileEditProfileIv.setImageURI(result.data?.data)
-            if(result.data != null) {
+            if (result.data != null) {
                 path = absolutelyPath(result.data?.data!!)
             }
         }
@@ -65,7 +89,11 @@ class MyPageProfileEditActivity : AppCompatActivity() {
             showEditProfileDialog()
         }
         binding.profileEditDoneBtn.setOnClickListener {
-            putMyProfileEditToServer() }
+            makeMultipartBody()
+            Handler(Looper.getMainLooper()).postDelayed({
+                putMyProfileEditToServer()
+            }, 1000)
+        }
     }
 
     private fun setEditTextWatcher() {
@@ -118,6 +146,44 @@ class MyPageProfileEditActivity : AppCompatActivity() {
                     this@MyPageProfileEditActivity,
                     object : CapinDialogButton.OnClickListener {
                         override fun onClick() {
+                            nicknameBody =
+                                MultipartBody.Part.createFormData("nickname", tempNickname!!)
+                            Glide.with(this@MyPageProfileEditActivity)
+                                .asBitmap().load(tempCafeti)
+                                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                                .skipMemoryCache(true)
+                                .listener(object : RequestListener<Bitmap?> {
+                                    override fun onLoadFailed(
+                                        @Nullable e: GlideException?,
+                                        o: Any?,
+                                        target: com.bumptech.glide.request.target.Target<Bitmap?>?,
+                                        b: Boolean
+                                    ): Boolean {
+                                        return false
+                                    }
+
+                                    override fun onResourceReady(
+                                        resource: Bitmap?,
+                                        model: Any?,
+                                        target: com.bumptech.glide.request.target.Target<Bitmap?>?,
+                                        dataSource: DataSource?,
+                                        isFirstResource: Boolean
+                                    ): Boolean {
+                                        tempFile = bitmapToFile(resource!!, "image.jpeg")
+                                        return true
+                                    }
+                                }
+                                ).submit()
+                            if (tempFile != null) {
+                                val requestBody: RequestBody =
+                                    tempFile!!.asRequestBody("image/jpeg".toMediaTypeOrNull())
+                                profileImageBody = MultipartBody.Part.createFormData(
+                                    "profileImg",
+                                    tempFile!!.name,
+                                    requestBody
+                                )
+                            }
+                            putMyProfileEditToServer()
                             getMyInfoFromServer()
                             dialog.dismiss()
                         }
@@ -149,6 +215,10 @@ class MyPageProfileEditActivity : AppCompatActivity() {
                         .with(binding.profileEditProfileIv.context)
                         .load(response.body()?.myInfo?.profileImg)
                         .into(binding.profileEditProfileIv)
+
+                    tempNickname = response.body()?.myInfo?.nickname
+                    tempCafeti = response.body()?.myInfo?.cafeti?.img
+                    tempExistingImage = response.body()?.myInfo?.profileImg
                 }
             }
         })
@@ -157,7 +227,7 @@ class MyPageProfileEditActivity : AppCompatActivity() {
     private fun chekGalleryPermission() {
         if (ContextCompat.checkSelfPermission(
                 this,
-                Companion.PERMISSION_READ_EXTERNAL_STORAGE
+                PERMISSION_READ_EXTERNAL_STORAGE
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             requestGalleryPermission.launch(PERMISSION_READ_EXTERNAL_STORAGE)
@@ -179,38 +249,75 @@ class MyPageProfileEditActivity : AppCompatActivity() {
             }
         }
 
+    private fun makeMultipartBody() {
+        nicknameBody = if (binding.profileEditNameEdt.text.isNullOrBlank()) {
+            MultipartBody.Part.createFormData(
+                "nickname",
+                binding.profileEditNameEdt.hint.toString()
+            )
+        } else {
+            MultipartBody.Part.createFormData(
+                "nickname",
+                binding.profileEditNameEdt.text.toString()
+            )
+        }
+
+        if (path == "") {
+            Glide.with(this)
+                .asBitmap()
+                .load(tempExistingImage)
+                .into(object: CustomTarget<Bitmap>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // 아래 resource가 들어간 뷰가 사라지는 등의 경우의 처리
+                    }
+
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        // 얻어낸 Bitmap 자원을 resource를 통하여 접근.
+                        tempFile = bitmapToFile(resource!!, "image.jpeg")
+                    }
+                })
+            if (tempFile != null) {
+                val requestBody: RequestBody =
+                    tempFile!!.asRequestBody("image/jpeg".toMediaTypeOrNull())
+                profileImageBody =
+                    MultipartBody.Part.createFormData("profileImg", tempFile!!.name, requestBody)
+            }
+        } else {
+            val file = File(path)
+            val requestBody: RequestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            profileImageBody =
+                MultipartBody.Part.createFormData("profileImg", file.name, requestBody)
+        }
+    }
+
     private fun putMyProfileEditToServer() {
-        val file = File(path)
-
-        var requestBody : RequestBody = RequestBody.create("image/jpeg".toMediaTypeOrNull(),file)
-        var body : MultipartBody.Part = MultipartBody.Part.createFormData("profileImg",file.name,requestBody)
-        var nicknameBody : MultipartBody.Part = MultipartBody.Part.createFormData("nickname", binding.profileEditNameEdt.text.toString())
-
         val capinApiService = ServiceCreator.capinApiService.putMyProfileEdit(
             userPreferenceManager.getUserAccessToken(),
             nickname = nicknameBody,
-            profileImg = body
+            profileImg = profileImageBody
         )
 
         capinApiService.enqueue(object : Callback<BaseResponse> {
             override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
                 Log.d("fail", "error:$t")
-                CapinToastMessage.createCapinRejectToast(
-                    this@MyPageProfileEditActivity,
-                    "사용할 수 없는 이름입니다.",
-                    100
-                )?.show()
+
             }
 
             override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
                 if (response.isSuccessful) {
                     finish()
+                } else {
+                    CapinToastMessage.createCapinRejectToast(
+                        this@MyPageProfileEditActivity,
+                        "사용할 수 없는 이름입니다.",
+                        100
+                    )?.show()
                 }
             }
         })
     }
 
-    fun absolutelyPath(path: Uri): String {
+    private fun absolutelyPath(path: Uri): String {
 
         var proj: Array<String> = arrayOf(MediaStore.Images.Media.DATA)
         var c: Cursor = contentResolver.query(path, proj, null, null, null)!!
@@ -220,6 +327,33 @@ class MyPageProfileEditActivity : AppCompatActivity() {
         var result = c.getString(index)
 
         return result
+    }
+
+    fun bitmapToFile(bitmap: Bitmap, fileNameToSave: String): File? { // File name like "image.png"
+        //create a file to write bitmap data
+        var file: File? = null
+        return try {
+            file = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + File.separator + fileNameToSave
+            )
+            file.createNewFile()
+
+            //Convert bitmap to byte array
+            val bos = ByteArrayOutputStream()
+            bitmap.compress(CompressFormat.JPEG, 0, bos) // YOU can also save it in JPEG
+            val bitmapdata = bos.toByteArray()
+
+            //write the bytes in file
+            val fos = FileOutputStream(file)
+            fos.write(bitmapdata)
+            fos.flush()
+            fos.close()
+            file
+        } catch (e: Exception) {
+            e.printStackTrace()
+            file // it will return null
+        }
     }
 
     companion object {
