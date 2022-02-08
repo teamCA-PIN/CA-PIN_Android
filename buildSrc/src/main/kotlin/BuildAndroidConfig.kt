63e5782895218413c890941044f906cffe77dbf2
sonpyeonghwa <ssrsph@naver.com>
2022-02-08 14:02:22 +0900

    [CHORE] 버전코드 수정

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -4,7 +4,7 @@ object BuildAndroidConfig {
     const val TARGET_SDK_VERSION = 31
     const val MIN_SDK_VERSION = 23
     const val BUILD_TOOLS_VERSION = "30.0.3"
-    const val VERSION_CODE = 5
+    const val VERSION_CODE = 6
     const val VERSION_NAME = "1.0.1"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
